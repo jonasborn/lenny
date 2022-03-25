4a246f2162a299e00171101cf9fb7aa664a903ea
themesh <mail@jonasborn.de>
2022-03-25 23:21:46 +0000

    Updated run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
--ta ac3 -s F:\Serien\MythBusters\S02 -t F:\Serien\MythBusters\S02 -ffprobe "C:\Windows\System32\ffprobe.exe" -ffmpeg "C:\Windows\System32\ffmpeg.exe" -tf mp4 -tv h264 -sal stereo -sa ac3 mp3 acc -sv h264 -do
+java -jar out/artifact/lenny.jar -ta ac3 -s "$1" -t "$2" -ffprobe "/usr/bin/ffprobe" -ffmpeg "/usr/bin/ffmpeg" -tf mp4 -tv h264 -sal stereo -sa ac3 mp3 acc -sv h264 -do
